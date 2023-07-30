0caac25b6e8b393858d9614d632cd3745ddada76
sanjueranga <sanjueranga16@gmail.com>
2023-07-30 21:58:02 +0530

    api

--- frontend/src/features/users/userService.js
+++ frontend/src/features/users/userService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://member-portal-api.onrender.com/student/';
+const API_URL = '/student/';
 
 //create new user
 const register = async (userData) => {
