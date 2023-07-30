b3a5469553975e3725f58449f5c67355fc524e12
sanjueranga <sanjueranga16@gmail.com>
2023-07-30 21:38:54 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser")
 const app = express();
-const cors = require('cors');
+
 
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
 
 //Import all routes
 const auth = require('./routes/authRoutes');
