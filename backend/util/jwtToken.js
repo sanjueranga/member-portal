9e06de44c0a316efd79a73dfe03670648be7e64d
sanjueranga <sanjueranga16@gmail.com>
2023-07-31 11:41:08 +0530

    user res

--- backend/util/jwtToken.js
+++ backend/util/jwtToken.js
@@ -12,7 +12,7 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(Date.now()  + 12* 60* 60 * 1000),
         httpOnly: true
     }
-
+    console.log(user);
     // // Save token and user data in the cookie
     res.status(statusCode)
         .cookie('token', token, options)
