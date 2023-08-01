91970eab888f1d47aad6e8fa0ac94191da638a2f
sanjueranga <sanjueranga16@gmail.com>
2023-08-01 12:30:00 +0530

    c see

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -2,10 +2,10 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import authService from './authService';
 import Cookies from 'js-cookie';
 
-const localUser = JSON.parse(Cookies.get('user'));
+// const localUser = JSON.parse(Cookies.get('user'));
 
 const initialState = {
-	user: localUser !== undefined ? localUser : null,
+	user: null,
 	isError: false,
 	isSuccess: false,
 	isLoading: false,
